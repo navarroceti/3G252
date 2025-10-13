3417cdc369fd7ed05941dc55bb9ed9a1607a7532
Jose Ramon Navarro <129806280+navarroceti@users.noreply.github.com>
2025-10-13 23:49:19 +0000

    base code 3G252

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Foco.hpp>
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    Foco foquito;
+    foquito.Encender();
+    foquito.Apagar();
+
+    
+    return 0;
+}
